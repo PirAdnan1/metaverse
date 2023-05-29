7f66de2a5cacd911ebc2ecb2aeb5c3edfb0d9964
PirAdnan1 <109432989+PirAdnan1@users.noreply.github.com>
2023-05-29 07:30:56 -0700

    add animations to picturse

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -7,17 +7,17 @@ import { navVariants } from "../utils/motion";
 
 const Navbar = () => (
   <motion.nav
-    className="px-6 sm:px-16 relative"
+    className='px-6 sm:px-16 relative'
     variants={navVariants}
-    initial="show"
-    whileInView="animate"
+    initial='show'
+    whileInView='animate'
     viewport={{once: true}}
   >
-    <div className="absolute inset-0 w-[50%] gradient-01" />
-    <div className="pt-8 flex justify-between items-center">
-      <img src="/search.svg" alt="search" />
-      <h1 className="text-white font-extrabold leading-7 text-3xl">METAVERSUS</h1>
-      <img src="/LogoVector.svg" alt="..." />
+    <div className='absolute inset-0 w-[50%] gradient-01' />
+    <div className='pt-8 flex justify-between items-center'>
+      <img src='/search.svg' alt='search' />
+      <h1 className='text-white font-extrabold leading-7 text-3xl'>METAVERSUS</h1>
+      <img src='/LogoVector.svg' alt='...' />
     </div>
   </motion.nav>
 );
