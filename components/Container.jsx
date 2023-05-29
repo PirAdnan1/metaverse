4f08beade86c07096c9448afbc643834e5405c9a
PirAdnan1 <109432989+PirAdnan1@users.noreply.github.com>
2023-05-29 05:41:12 -0700

    Fixed container issue

--- components/Container.jsx
+++ components/Container.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Container({children}) {
   return (
-    <div className=' max-w-[1636px] mx-auto '>{children}</div>
+    <div className='max-w-[1636px] mx-auto'>{children}</div>
   )
 }
 
